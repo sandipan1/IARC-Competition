c027d0883e549fe816a733b67605eefd7c3ded38
Sandipan Haldar <sandipanh05@gmail.com>
2017-03-07 21:23:16 +0530

    Update iarc.py

--- iarc.py
+++ iarc.py
@@ -3,13 +3,18 @@ from math import *
 import numpy as np 
 im=cv2.imread("/home/sandipan/computer_vision/bellatrix/1.jpg")
 
-gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY) 
 #blurred = cv2.GaussianBlur(gray,(5,5),0)
-ret,thresh = cv2.threshold(gray,127,255,cv2.THRESH_TRUNC)
+#ret,thresh = cv2.threshold(gray,80,255,cv2.THRESH_BINARY)
+
+# Final Adaptive thresholding
+thresh1= cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,25,10)
+
 #thresh= cv2.adaptiveThreshold(blurred,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
 kernel=np.ones((5,5),np.uint8)
-closing = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
-
+#erosion = cv2.erode(thresh1,kernel,iterations = 1)
+closing = cv2.morphologyEx(thresh1, cv2.MORPH_CLOSE, kernel)
+cv2.imshow('closing',closing)
 gr=closing.astype(float)
 
 height,width,channel=im.shape
@@ -17,8 +22,11 @@ y=height-75
 x1= 15
 x2= width -100
 print y,x1,x2
+# taking a line
 line=[gr[y][i] for i in range (x1,x2)]
+#finding derivative
 der= [line[i+1]-line[i] for i in range (len(line)-1)]
+# abs value of derivative
 der = [abs(x) for x in der]
 der_thres=[der[i]>40 for i in range(len(der))]
 edge_pts=[]
@@ -28,8 +36,9 @@ for i in range(len(der_thres)):
 print edge_pts
 
 for i in edge_pts:
-	cv2.circle(closing,(i,y), 5, (0,255,0), -1)
-cv2.imshow('frame',closing)
-cv2.imshow("Thresholded",thresh)
-cv2.imshow('closing',closing)
+	cv2.circle(closing,(i+1,y), 5, (0,255,0), -1)
+#cv2.imshow('frame',closing)
+#cv2.imshow('g',gray)
+#cv2.imshow("Thresholded",thresh)
+#cv2.imshow('closing',closing)
 cv2.waitKey(0)
